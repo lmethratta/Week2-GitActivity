3e16f619787159695fff041109dda412236811be
lmethratta <leahmethratta@gmail.com>
2024-09-13 11:23:39 -0400

    updated to include subtract method

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,6 +2,10 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
+  public subtract(x: number, y: number) : number {
+    return x - y;
+  }
+
   public multiply(x: number, y: number) : number {
     return x * y;
   }
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-console.log(`result_add = ${result_add}`);
-
+const result_subtract = calc.subtract(3, 2);
 const result_multiply = calc.multiply(2, 3);
+console.log(`result_add = ${result_add}`);
+console.log(`result_subtract = ${result_subtract}`);
 console.log(`result_multiply = ${result_multiply}`);
+
+
 
